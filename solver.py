9e1beebc21ac0aabaafbf78865dbc91444275d8a
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2023-10-04 20:30:21 -0400

    created solver for aligning the corners

--- solver.py
+++ solver.py
@@ -3,7 +3,10 @@ import numpy as np
 from cube import Cube, Cube3x3
 from enums import Color, Face
 from error import ImpossibleScrambleException
-from utils import clean_moves
+from utils import clean_moves, sexy_move_times
+
+SIDE_FACES = [Face.FRONT, Face.LEFT, Face.BACK, Face.RIGHT]
+SIDE_FACE_PAIRS = [*zip(SIDE_FACES[:4], SIDE_FACES[1:4] + SIDE_FACES[0:1])]
 
 def orient_centers(cube: Cube3x3) -> list[str]:
     """ 
@@ -21,18 +24,15 @@ def orient_centers(cube: Cube3x3) -> list[str]:
         cube.turn(direction, 1, 2, 1, moves)
     return clean_moves(moves)
 
-def solve_cross(cube: Cube3x3) -> list[str]:
+def solve_white_cross(cube: Cube3x3) -> list[str]:
     """
     Solves the cross for a cube, assuming centers are oriented.
     First, get all the white edges oriented correctly on the top face.
     Then, insert each edge into the correct place using 180 turns.
     """
 
-    cube_matrix = cube.get_cube()
-    side_faces = [Face.FRONT, Face.LEFT, Face.BACK, Face.RIGHT]
-    side_face_pairs = [*zip(side_faces[:4], side_faces[1:4] + side_faces[0:1])]
-    def all_edges_white(cube_matrix: list[np.ndarray], face: Face) -> bool:
-        face_array = cube_matrix[face.value]
+    def all_edges_white(cube: Cube3x3, face: Face) -> bool:
+        face_array = cube.get_cube()[face.value]
         return all([
             face_array[*indeces] == Color.WHITE for indeces
             in [(0, 1), (1, 0), (1, 2), (2, 1)]
@@ -40,7 +40,7 @@ def solve_cross(cube: Cube3x3) -> list[str]:
 
     moves = []
     safety_counter = 0
-    while not all_edges_white(cube_matrix, Face.TOP):
+    while not all_edges_white(cube, Face.TOP):
         safety_counter += 1
         if safety_counter > 100:
             raise ImpossibleScrambleException("Cube could not be solved.")
@@ -51,14 +51,14 @@ def solve_cross(cube: Cube3x3) -> list[str]:
             top_front_edge = cube.get_edge_between(Face.FRONT, Face.TOP)
         if top_front_edge["f2c"][Face.FRONT] == Color.WHITE:
             cube.parse("F U' R", output_movelist=moves)
-        elif any((l:=[Color.WHITE in cube.get_edge_between(i, j)["c2f"] for i, j in side_face_pairs])):
+        elif any((l:=[Color.WHITE in cube.get_edge_between(i, j)["c2f"] for i, j in SIDE_FACE_PAIRS])):
             loc = l.index(True)
             cube.turn("U", -loc, 2, 1, moves)
             if cube.get_edge_between(Face.FRONT, Face.LEFT)["f2c"][Face.FRONT] == Color.WHITE:
                 cube.parse("2U' F'", output_movelist=moves)
             else:
                 cube.turn('F', 1, 1, 1, moves)
-        elif any((l:=[Color.WHITE in cube.get_edge_between(Face.BOTTOM, i)["c2f"] for i in side_faces])):
+        elif any((l:=[Color.WHITE in cube.get_edge_between(Face.BOTTOM, i)["c2f"] for i in SIDE_FACES])):
             loc = l.index(True)
             cube.turn("D", loc, 1, 1, moves)
             if cube.get_edge_between(Face.FRONT, Face.BOTTOM)["f2c"][Face.BOTTOM] == Color.WHITE:
@@ -66,7 +66,7 @@ def solve_cross(cube: Cube3x3) -> list[str]:
             else:
                 cube.parse("F' U' R", output_movelist=moves)
 
-    while not all_edges_white(cube_matrix, Face.BOTTOM):
+    while not all_edges_white(cube, Face.BOTTOM):
         safety_counter += 1 
         if safety_counter > 200:
             raise ImpossibleScrambleException("Cube could not be solved")
@@ -81,9 +81,90 @@ def solve_cross(cube: Cube3x3) -> list[str]:
 
     return clean_moves(moves)
 
+def solve_first_layer_corners(cube: Cube3x3) -> list[str]:
+    """
+    Solves the corners for the first layer of the cube.
+    Assumes the cross is already properly built.
+    """
+    def all_white_corners_solved(cube: Cube3x3) -> bool:
+        """
+        Determines if all the corners of the white face are solved.
+        """
+        for right, left in SIDE_FACE_PAIRS: 
+            if not all(is_corner_solved(cube, left, right).values()):
+                return False
+        return True
+
+    def is_corner_solved(cube: Cube3x3, a: Face, b: Face) -> dict[str, bool]:
+        """
+        Determines if a single corner is in a solved position.
+        Returns a dictionary saying if:
+            It's permuted correctly - in the right place
+            It's oriented correctly - white is on bottom
+        """
+        corner = cube.get_corner_between(a, b, Face.BOTTOM)
+        return {
+            "permuted": (
+                cube.get_edge_between(a, Face.BOTTOM)["f2c"][a] in corner["c2f"] and 
+                cube.get_edge_between(b, Face.BOTTOM)["f2c"][b] in corner["c2f"] and 
+                Color.WHITE in corner["c2f"]
+            ),
+            "oriented": (
+                corner["f2c"][Face.BOTTOM] == Color.WHITE and 
+                corner["f2c"][a] == cube.get_edge_between(a, Face.BOTTOM)["f2c"][a] and 
+                corner["f2c"][b] == cube.get_edge_between(b, Face.BOTTOM)["f2c"][b]
+            )
+        }
+
+    def is_corner_matched(cube: Cube3x3, faces: list[Face], colors: list[Color]) -> bool:
+        """
+        Determines if a given corner is composed of given colors
+        """
+        corner = cube.get_corner_between(*faces)
+        return all([
+            color in corner["c2f"] for color in colors
+        ])
+
+    moves = []
+    while not all_white_corners_solved(cube):
+        while all(is_corner_solved(cube, Face.FRONT, Face.RIGHT).values()):
+            cube.turn("D", 1, 2, 2, moves)
+        right_color = cube.get_edge_between(Face.RIGHT, Face.BOTTOM)["f2c"][Face.RIGHT]
+        front_color = cube.get_edge_between(Face.FRONT, Face.BOTTOM)["f2c"][Face.FRONT] 
+        if is_corner_solved(cube, Face.FRONT, Face.RIGHT)["permuted"]:
+            match cube.get_corner_between(Face.FRONT, Face.RIGHT, Face.BOTTOM)["c2f"][Color.WHITE]:
+                case Face.RIGHT: sexy_moves = 2
+                case Face.FRONT: sexy_moves = 4
+                case _: raise ImpossibleScrambleException("Cube could not be solved.")
+            continue
+        if any((l:=[
+            is_corner_matched(cube, [Face.BOTTOM, a, b], [right_color, front_color, Color.WHITE])
+            for a, b in SIDE_FACE_PAIRS
+        ])):
+            loc = l.index(True) + 1
+            cube.turn('D', loc, 1, 1, moves)
+            cube.parse(sexy_move_times(1), output_movelist=moves)
+            cube.turn('D', -loc, 1, 1, moves)
+        if any((l:=[
+            is_corner_matched(cube, [Face.TOP, a, b], [right_color, front_color, Color.WHITE])
+            for a, b in SIDE_FACE_PAIRS
+        ])):
+            loc = l.index(True) + 1
+            cube.turn('U', -loc, 1, 1, moves)
+            match cube.get_corner_between(Face.FRONT, Face.RIGHT, Face.TOP)["c2f"][Color.WHITE]:
+                case Face.TOP: sexy_moves = 3
+                case Face.FRONT: sexy_moves = 5
+                case Face.RIGHT: sexy_moves = 1
+                case _: raise ImpossibleScrambleException("Cube could not be solved.")
+            cube.parse(sexy_move_times(sexy_moves), output_movelist=moves)
+    
+    return moves
+
 if __name__ == "__main__":
     cube = Cube.from_commandline()
     assert isinstance(cube, Cube3x3)
+    print(cube)
     print(orient_centers(cube))
-    print(solve_cross(cube))
+    print(solve_white_cross(cube))
+    print(solve_first_layer_corners(cube))
     print(cube)
