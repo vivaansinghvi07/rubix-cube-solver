61f5efb8aa36e940a0d2377b75f3451adc8d5968
vivaansinghvi07 <vivaansinghvi8@gmail.com>
2023-11-13 23:56:20 -0500

    added new error type

--- pycubing/error.py
+++ pycubing/error.py
@@ -7,3 +7,8 @@ class InvalidTurnException(Exception):
     """ Exception raised when the turn cannot be made """
     def __init__(self, message: str) -> None:
         self.message = message
+
+class ComputerVisionException(Exception):
+    """ Exceptions related to the computer vision part of the project """
+    def __init__(self, message: str) -> None:
+        self.message = message
